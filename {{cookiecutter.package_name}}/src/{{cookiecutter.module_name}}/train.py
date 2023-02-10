621e595f825f676dc5680e28fe0b691ccfa8f486
Vincent Emonet <vincent.emonet@gmail.com>
2023-02-10 13:23:22 +0100

    remove fairworkflow 🥳

--- {{cookiecutter.package_name}}/src/{{cookiecutter.module_name}}/train.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.module_name}}/train.py
@@ -1,7 +1,6 @@
 import logging
 
 import typer
-from fairworkflows import FairWorkflow, is_fairstep, is_fairworkflow
 from openpredict import save
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
@@ -10,44 +9,12 @@ log = logging.getLogger()
 
 cli = typer.Typer(help="Training for models")
 
-@cli.command(help='Train a model')
-def train_model(n_jobs = 2):
-    model_id = "{{cookiecutter.module_name}}"
 
-    workflow = FairWorkflow.from_function(training_workflow)
-    result, prov = workflow.execute(n_jobs)
-    # This does not really execute the workflow, only analyzes it
-
-    log.info("Publishing to Nanopub test server")
-    workflow.publish_as_nanopub(use_test_server=True, publish_steps=True)
-    prov.publish_as_nanopub(use_test_server=True)
-
-    # Actually run the training workflow
-    loaded_model = training_workflow(n_jobs)
-
-    workflow._rdf.serialize(f"models/{model_id}.workflow.trig", format="trig")
-    prov._rdf.serialize(f"models/{model_id}.prov.trig", format="trig")
-
-    return loaded_model
-
-
-@is_fairstep(label='Load data', is_script_task=True)
 def load_data():
     data, y = load_iris(return_X_y=True, as_frame=True)
-    return data
-    # return {
-    #     "data": data,
-    #     "y": y
-    # }
+    return data, y
 
-@is_fairstep(label='Load data y', is_script_task=True)
-def load_data_y():
-    # We need to duplicate the use of load_iris because FAIRworkflow
-    # doesn't support returning tuples or objects
-    data, y = load_iris(return_X_y=True, as_frame=True)
-    return y
 
-@is_fairstep(label='Create and fit classifier', is_script_task=True)
 def fit_classifier(hyper_params, data, y):
     clf = RandomForestClassifier(
         n_jobs=hyper_params['n_jobs'],
@@ -57,7 +24,6 @@ def fit_classifier(hyper_params, data, y):
     return clf
 
 
-@is_fairstep(label='Evaluate the trained model', is_script_task=True)
 def evaluate(model):
     # Evaluate the quality of your model using custom metrics
     # cf. https://scikit-learn.org/stable/modules/model_evaluation.html
@@ -71,7 +37,6 @@ def evaluate(model):
     }
 
 
-@is_fairstep(label='Save the trained model', is_script_task=True)
 def save_model(model, path, sample_data, scores, hyper_params):
     loaded_model = save(
         model,
@@ -83,7 +48,7 @@ def save_model(model, path, sample_data, scores, hyper_params):
     return loaded_model
 
 
-@is_fairworkflow(label='{{cookiecutter.package_name_stylized}} training workflow')
+@cli.command(help='Train a model')
 def training_workflow(n_jobs: int):
 
     # Define models hyper params
@@ -92,8 +57,7 @@ def training_workflow(n_jobs: int):
         'random_state': 42
     }
 
-    data = load_data()
-    y = load_data_y()
+    data, y = load_data()
 
     # Train model (here we use a stub dataset just to make the example clear)
     model = fit_classifier(hyper_params, data, y)
